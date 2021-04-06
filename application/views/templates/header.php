2f8bfcb4c5e81de653a5cfbb237b1df3d488a9c2
Michael Perron <michael.perron@g.austincc.edu>
2021-04-05 20:26:09 -0500

    corrected the href link for the bootstrap.min.css file

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,7 +3,7 @@
 			<title>CodeIgniter Tutorial</title>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-			<link href="/css/bootstrap.min.css" rel="stylesheet">
+			<link href="assets/css/bootstrap.min.css" rel="stylesheet">
 			<link href="assets/css/main.css" rel="stylesheet">
 			<link rel="icon" href="favicon.ico" type="image/gif">
 		</head>
