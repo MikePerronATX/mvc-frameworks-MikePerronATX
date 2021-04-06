362e32475a79bb5f7d0eb9b311a88c8d634f84f3
Michael Perron <michael.perron@g.austincc.edu>
2021-04-05 19:28:27 -0500

    added links and meta data in the head for images and css.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,11 @@
 <html>
         <head>
 			<title>CodeIgniter Tutorial</title>
-			
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<link href="/css/bootstrap.min.css" rel="stylesheet">
+			<link href="assets/css/main.css" rel="stylesheet">
+			<link rel="icon" href="favicon.ico" type="image/gif">
 		</head>
         <body>
-
-                <?= $title; ?>
+            <?= $title; ?>
