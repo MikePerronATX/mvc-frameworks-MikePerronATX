7f496ec98e996d882f2f87e2b2a9936504058f89
Michael Perron <michael.perron@g.austincc.edu>
2021-04-06 22:31:19 -0500

    added top comment, fixed favicon image, and added nav menu.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,3 +1,8 @@
+<!--
+Name: Michael Perron
+Coding 07
+Purpose: Fill-In.
+-->
 <html>
         <head>
 			<title>CodeIgniter Tutorial</title>
@@ -5,7 +10,23 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 			<link href="assets/css/bootstrap.min.css" rel="stylesheet">
 			<link href="assets/css/main.css" rel="stylesheet">
-			<link rel="icon" href="favicon.ico" type="image/gif">
+			<link rel="icon" href="/assets/images/favicon.ico" type="image/gif">
 		</head>
         <body>
-            <?= $title; ?>
+		<?= $title; ?>
+			<nav class="navbar navbar-expand-md">
+				<a class="navbar-brand" href="/home">Easter Home</a>
+				<button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="main-navigation">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="/home">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/about">About</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
