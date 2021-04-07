2125623de52bd287218d556d13314fcc20587dd0
Michael Perron <michael.perron@g.austincc.edu>
2021-04-06 22:36:54 -0500

    emoved the single-word placeholder and added text, headers, images, and js for the nav menu

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,92 @@
-<p>About</p>
+<!--
+Name: Michael Perron
+Coding 07
+Purpose: Fill-In.
+-->
+
+<div class="body-div">
+    
+        <img src="/assets/images/cartonEgg.jpg" class="img-fluid">
+    
+    <div class="egg-text">
+	<h1 class="mainTitleColor">HOW TO DYE EASTER EGGS WITHOUT A KIT</h1>
+
+            Skip the Easter egg decorating kit this year. 
+            Here’s how make your own egg dye in minutes, using things you probably already 
+            have in your pantry.
+
+		<h3 class="eggTitles">How to Dye Easter Eggs Without a Kit</h3>
+			
+            Ingredients:
+            Hard-boiled eggs
+            Food coloring
+            White vinegar
+
+            If You Don’t Have Vinegar: Use lemon juice in place of the vinegar, or just leave the vinegar out. 
+            Eggs dyed without vinegar will turn out pastel-colored. You need a mild acid, like vinegar or 
+            lemon juice to achieve really vibrant colors.
+
+		<h3 class="eggTitles">What You Do:</h3>
+
+            Bring a large pot of water to a boil.
+
+            <img src="/assets/images/boilEgg.jpg" class="img-fluid">
+			
+		<h3 class="eggTitles">Homemade Easter Egg Dyes</h3>
+
+            Measure 1/2 cup of the boiling water into a canning jar or another heat-resistant container. 
+            Then, add one teaspoon of vinegar and 10-20 drops of food coloring to achieve your desired color. 
+            Repeat the process to create additional colors. We used neon food coloring to make most of the 
+            colors that you see here. If you’re working with small kids, allow the dye to cool before you 
+            move on to dying eggs.
+
+            <img src="/assets/images/dipEgg.jpg" class="img-fluid">
+			
+		<h3 class="eggTitles">Eggs in Homemade Dyes</h3>
+			
+            Place eggs in your dye, and allow them to soak for around five minutes. You’ll probably need 
+            to flip your eggs partway through, so that both sides are evenly colored.
+
+            <img src="/assets/images/flipEgg.jpg" class="img-fluid">
+
+        <h3 class="eggTitles">Carton of Eggs Dyed Without a Kit</h3>
+			
+            To give you an idea of what sorts of colors you can achieve, we dipped a brown egg and a white 
+            egg in each color. Those are brown eggs on the right side of the carton and white eggs on the 
+            left side.
+
+            <img src="/assets/images/designEgg.jpg" class="img-fluid">
+
+        <h3 class="eggTitles">Egg Covered in Stickers</h3>
+			
+            If you want to get fancy, you can do some color-blocking by placing stickers on the eggs before 
+            you drop them into the dye. We had some spring-themed foam stickers on hand, so that’s what we 
+            used. Regular stickers would work just as well.
+
+            <img src="/assets/images/finishedEgg.jpg" class="img-fluid">
+
+        <h3 class="eggTitles">Dyed Eggs Before Stickers Were Removed</h3>
+
+            Here’s what the eggs looked like when we removed them from the dye baths.
+
+            <img src="/assets/images/closeUpEgg.jpg" class="img-fluid">
+        
+        <h3 class="eggTitles">Egg with Stickers Removed</h3>
+
+        And here’s an example of what they looked like after we peeled off the stickers.
+
+        <h3 class="eggTitles">How to Store Your Dyed Easter Eggs</h3>
+
+        Place your finished eggs back in their carton, and allow them to dry completely, before you move 
+        them to the refrigerator.
+
+        <h3 class="eggTitles">Tip:</h3>
+
+        When you take your eggs out of the fridge, allow them to dry before you handle them. This 
+will keep the colors from smearing or running.
+
+    </div>    
+</div>
+
+<script src=assets/js/jquery.min.js></script>
+<script src="assets/js/bootstrap.min.js"></script>
