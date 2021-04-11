d3b84a17debcda5d90ce809b605167660615fe08
Michael Perron <michael.perron@g.austincc.edu>
2021-04-11 12:54:30 -0500

    several changes made on is page. Added js scripts at the bottom of the page. Added a form since forms will be discussed in the next assignment and I wanted to make this page a little more intersting than just text. Also, experimented with the html structure for UI and added additional images.

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -3,90 +3,111 @@ Name: Michael Perron
 Coding 07
 Purpose: Fill-In.
 -->
-
 <div class="body-div">
-    
-        <img src="/assets/images/cartonEgg.jpg" class="img-fluid">
-    
+    <img src="/assets/images/cartonEgg.jpg" class="img-fluid" id="eggs">
     <div class="egg-text">
-	<h1 class="mainTitleColor">HOW TO DYE EASTER EGGS WITHOUT A KIT</h1>
+        <h1 class="mainTitleColor">HOW TO DYE EASTER EGGS WITHOUT A KIT</h1>
+        Skip the Easter egg decorating kit this year. 
+        Here’s how make your own egg dye in minutes, using things you probably already 
+        have in your pantry.
 
-            Skip the Easter egg decorating kit this year. 
-            Here’s how make your own egg dye in minutes, using things you probably already 
-            have in your pantry.
+        <h3 class="titleColorAbout">Dye Easter Eggs Without a Kit</h3>
 
-		<h3 class="eggTitles">How to Dye Easter Eggs Without a Kit</h3>
-			
-            Ingredients:
-            Hard-boiled eggs
-            Food coloring
-            White vinegar
+        <p>Ingredients:
+        Hard-boiled eggs
+        Food coloring
+        White vinegar
+        If You Don’t Have Vinegar: Use lemon juice in place of the vinegar, or just leave the vinegar out. 
+        Eggs dyed without vinegar will turn out pastel-colored. You need a mild acid, like vinegar or 
+        lemon juice to achieve really vibrant colors. Bring a large pot of water to a boil.</p>
 
-            If You Don’t Have Vinegar: Use lemon juice in place of the vinegar, or just leave the vinegar out. 
-            Eggs dyed without vinegar will turn out pastel-colored. You need a mild acid, like vinegar or 
-            lemon juice to achieve really vibrant colors.
+        <img src="/assets/images/boilEgg.jpg" class="img-fluid" id="eggs">
 
-		<h3 class="eggTitles">What You Do:</h3>
+        <h3 class="titleColorAbout">Homemade Easter Egg Dyes</h3>
 
-            Bring a large pot of water to a boil.
+        <p>Measure 1/2 cup of the boiling water into a canning jar or another heat-resistant container. 
+        Then, add one teaspoon of vinegar and 10-20 drops of food coloring to achieve your desired color. 
+        Repeat the process to create additional colors. We used neon food coloring to make most of the 
+        colors that you see here. If you’re working with small kids, allow the dye to cool before you 
+        move on to dying eggs.</p>
 
-            <img src="/assets/images/boilEgg.jpg" class="img-fluid">
-			
-		<h3 class="eggTitles">Homemade Easter Egg Dyes</h3>
+        <img src="/assets/images/dipEgg.jpg" class="img-fluid" id="eggs">
 
-            Measure 1/2 cup of the boiling water into a canning jar or another heat-resistant container. 
-            Then, add one teaspoon of vinegar and 10-20 drops of food coloring to achieve your desired color. 
-            Repeat the process to create additional colors. We used neon food coloring to make most of the 
-            colors that you see here. If you’re working with small kids, allow the dye to cool before you 
-            move on to dying eggs.
+        <h3 class="titleColorAbout">Eggs in Homemade Dyes</h3>
 
-            <img src="/assets/images/dipEgg.jpg" class="img-fluid">
-			
-		<h3 class="eggTitles">Eggs in Homemade Dyes</h3>
-			
-            Place eggs in your dye, and allow them to soak for around five minutes. You’ll probably need 
-            to flip your eggs partway through, so that both sides are evenly colored.
-
-            <img src="/assets/images/flipEgg.jpg" class="img-fluid">
-
-        <h3 class="eggTitles">Carton of Eggs Dyed Without a Kit</h3>
-			
-            To give you an idea of what sorts of colors you can achieve, we dipped a brown egg and a white 
-            egg in each color. Those are brown eggs on the right side of the carton and white eggs on the 
-            left side.
-
-            <img src="/assets/images/designEgg.jpg" class="img-fluid">
-
-        <h3 class="eggTitles">Egg Covered in Stickers</h3>
-			
-            If you want to get fancy, you can do some color-blocking by placing stickers on the eggs before 
-            you drop them into the dye. We had some spring-themed foam stickers on hand, so that’s what we 
-            used. Regular stickers would work just as well.
-
-            <img src="/assets/images/finishedEgg.jpg" class="img-fluid">
-
-        <h3 class="eggTitles">Dyed Eggs Before Stickers Were Removed</h3>
-
-            Here’s what the eggs looked like when we removed them from the dye baths.
-
-            <img src="/assets/images/closeUpEgg.jpg" class="img-fluid">
+        <p>Place eggs in your dye, and allow them to soak for around five minutes. You’ll probably need 
+        to flip your eggs partway through, so that both sides are evenly colored.</p>
         
-        <h3 class="eggTitles">Egg with Stickers Removed</h3>
+        <img src="/assets/images/flipEgg.jpg" class="img-fluid" id="eggs">
 
-        And here’s an example of what they looked like after we peeled off the stickers.
+        <h3 class="titleColorAbout">Carton of Eggs Dyed Without a Kit</h3>
 
-        <h3 class="eggTitles">How to Store Your Dyed Easter Eggs</h3>
+        <p>To give you an idea of what sorts of colors you can achieve, we dipped a brown egg and a white 
+        egg in each color. Those are brown eggs on the right side of the carton and white eggs on the 
+        left side.</p>
 
-        Place your finished eggs back in their carton, and allow them to dry completely, before you move 
-        them to the refrigerator.
+        <img src="/assets/images/designEgg.jpg" class="img-fluid" id="eggs">
 
-        <h3 class="eggTitles">Tip:</h3>
+        <h3 class="titleColorAbout">Egg Covered in Stickers</h3>
 
-        When you take your eggs out of the fridge, allow them to dry before you handle them. This 
-will keep the colors from smearing or running.
+        <p>If you want to get fancy, you can do some color-blocking by placing stickers on the eggs before 
+        you drop them into the dye. We had some spring-themed foam stickers on hand, so that’s what we 
+        used. Regular stickers would work just as well.</p>
 
-    </div>    
+        <img src="/assets/images/finishedEgg.jpg" class="img-fluid" id="eggs">
+
+        <h3 class="titleColorAbout">Eggs Before Stickers Removed</h3>
+
+        <p>Here’s what the eggs looked like when we removed them from the dye baths.</p>
+        
+        <img src="/assets/images/closeUpEgg.jpg"  class="img-fluid" id="eggs">
+
+        <h3 class="titleColorAbout">Egg with Stickers Removed</h3>
+
+        <p>And here’s an example of what they looked like after we peeled off the stickers.</p>
+        
+        <h3 class="titleColorAbout">How to Store Your Dyed Easter Eggs</h3>
+
+        <p>Place your finished eggs back in their carton, and allow them to dry completely, before you move 
+        them to the refrigerator.</p>
+
+        <h3 id="tipColor">Tip:</h3>
+
+        <p id="tipColor-text">When you take your eggs out of the fridge, allow them to dry before you handle them. This 
+        will keep the colors from smearing or running.</p>
+
+        <h3 class="titleColorAbout">For More Information:</h3>
+
+        <div class="form">
+        <form id="email-form" name="email-form">
+            <div class="col-sm-12">Name
+                <input type="text" id="name" placeholder="name" name="name" maxlength="64"><br>
+            </div>
+            <div class="col-sm-12">Subject
+                <input type="text" id="subject" placeholder="subject" name="subject" maxlength="64"><br>
+            </div>
+            <div class="col-sm-12">Email
+                <input type="text" id="from" placeholder="email" name="from" maxlength="64"><br>
+            </div>
+            <div class="col-sm-12">Confirm Email
+                <input type="text" id="re_from" placeholder="confirm email" name="re_from" maxlength="64"><br>
+            </div>
+            <div class="col-sm-12">Message
+                <textarea class="form-control rounded-0" id="message" placeholder="message" name="message" rows="3" maxlength="1000"></textarea>
+            </div>
+            <div class="input-div">
+                <button id="names-send" type="button" class="">Send</button>
+                <button id="names-clear" type="button" class="">Clear</button>
+            </div>
+        </form>
+        <div id="msg">
+            <!-- message appears here -->
+            <br>
+        </div>
+   </div>    
+    </div>   
 </div>
-
 <script src=assets/js/jquery.min.js></script>
-<script src="assets/js/bootstrap.min.js"></script>
+<script src=assets/js/main.js></script>
+<script src=assets/js/email.js></script>
+<script src=assets/js/bootstrap.min.js></script>
