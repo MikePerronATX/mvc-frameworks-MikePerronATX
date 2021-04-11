2dd156890c17bfa0287a521eb14a7f64cc5c23ca
Michael Perron <michael.perron@g.austincc.edu>
2021-04-11 12:48:46 -0500

    added a $title variable and $servertime variable to display the page name and server time for each page didplayed

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,6 +3,11 @@ Name: Michael Perron
 Coding 07
 Purpose: Fill-In.
 -->			
+		<footer class="centered">
+        <?= $title; ?>
+        <br>
+        <?= $servertime; ?>
+        </footer>
                         <em>&copy; 2021</em>
         </body>
 </html>
