9d4d2e93622ff49830894f22b0681daa6c476fa0
Michael Perron <michael.perron@g.austincc.edu>
2021-04-26 20:43:30 -0500

    removed unneeded scripts from bottom of page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -84,6 +84,4 @@ Purpose: The about page on how to dye Easyter Eggs.
     </div>   
 </div>
 <script src=assets/js/jquery.min.js></script>
-<script src=assets/js/main.js></script>
-<script src=assets/js/email.js></script>
 <script src=assets/js/bootstrap.min.js></script>
