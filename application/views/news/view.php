986f4adc4f713cfac42d121b5bfc027a4bfd8f32
Michael Perron <michael.perron@g.austincc.edu>
2021-04-26 20:42:14 -0500

    added bootstrap and jquery to bottom of the page

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,2 +1,4 @@
 <?= '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
+echo $news_item['text'];?>
+<script src=/assets/js/jquery.min.js></script>
+<script src=/assets/js/bootstrap.min.js></script>
