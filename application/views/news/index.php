6d1ca0e2ce9375d9354d9a102f6209a0bbca3524
Michael Perron <michael.perron@g.austincc.edu>
2021-05-01 09:02:11 -0500

    experimenting with the index page. Adding code from the links provided in the assignment and will modify

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -5,6 +5,21 @@ Purpose: To display news clips
 -->
 <h2><?= $title; ?></h2>
 
+<div>
+	<h3>Home Page</h3>
+	<a href="/stories">Go to stories page</a><br>
+	<br>
+	<a href="story/1">Go to story 1 page</a><br>
+	<br>
+	<a href="story/2">Go to story 2 page</a><br>
+	<br>
+	<a href="story/3">Go to story 3 page</a><br>
+	<br>
+</div>
+
+
+
+
 <?php foreach ($news as $news_item): ?>
 
         <h3><?= $news_item['title']; ?></h3>
