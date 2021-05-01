6662c50c8125324c0dd57432b3707b634b4efa3d
Michael Perron <michael.perron@g.austincc.edu>
2021-05-01 09:05:40 -0500

    made a very small change on line 6 (removed an extra space)

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -8,9 +8,9 @@ class News extends CI_Controller {
 
         public function __construct()
         {
-                parent::__construct();
-                $this->load->model('news_model');
-                $this->load->helper('url_helper');
+            parent::__construct();
+            $this->load->model('news_model');
+            $this->load->helper('url_helper');
         }
         public function view($slug = NULL)
 		{
