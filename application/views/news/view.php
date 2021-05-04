afaac8feea56d9eff2cc348fbdebbe4a501497c2
Michael Perron <michael.perron@g.austincc.edu>
2021-05-03 22:26:44 -0500

    removing php structure and adding html structure by using parser library

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,2 +1,10 @@
-<?= '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
+<div class="viewDes">
+    {title}
+</div>
+<div class="viewText">
+    {text}
+</div>
+<a href="/news">Go back to News Archive</a><br>
+
+<script src=/assets/js/jquery.min.js></script>
+<script src=/assets/js/bootstrap.min.js></script>
