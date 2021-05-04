41fe7e6989aa31a989f3cd44925380848d3af6d0
Michael Perron <michael.perron@g.austincc.edu>
2021-05-03 22:18:53 -0500

    added the required Basepath and Forbidden line of code to meet specs

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -1,9 +1,10 @@
 <?php
+defined('BASEPATH') OR exit('Forbidden');
 class News_model extends CI_Model {
 
 	public function __construct()
 	{
-			$this->load->database();
+		$this->load->database();
 	}
 	public function get_news($slug = FALSE) {
         if ($slug === FALSE)
