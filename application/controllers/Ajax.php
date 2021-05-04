db6a77d67689e3ff4f4bd487631c7e891b8e0130
Michael Perron <michael.perron@g.austincc.edu>
2021-05-03 22:36:32 -0500

    adjusted code to allow emails to be sent. Kept having errors until I realized that my variable names were incorrect

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -9,25 +9,24 @@ class Ajax extends CI_Controller {
             // validate your data!
             $contactName = strip_tags(trim($this->input->post('contactName')));
             $contactFrom = strip_tags(trim($this->input->post('contactFrom')));
-            $contactRe_from = strip_tags(trim($this->input->post('contactRe_from')));
             $contactSub = strip_tags(trim($this->input->post('contactSub')));
             $contactMess = strip_tags(trim($this->input->post('contactMess')));
 
-            echo "okay";
-            $from = filter_var($from, FILTER_VALIDATE_EMAIL);
+            
+            $contactFrom = filter_var($contactFrom, FILTER_VALIDATE_EMAIL);
            
             // use and/or process your data
-            if(!empty($contactName) && !empty($contactFrom) && !empty($contactRe_from) && !empty($contactSub) && !empty($contactMess))
+            if(!empty($contactName) && !empty($contactFrom) && !empty($contactSub) && !empty($contactMess))
             {
             
-                $headers = "From: $from\r\n";
-                $headers .= "Reply-To: $from\r\n";
+                $headers = "From: $contactFrom\r\n";
+                $headers .= "Reply-To: $contactFrom\r\n";
                 $headers .= "MIME-Version: 1.0\r\n";
                 $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
     
-                $to = $from;
+                $to = $contactFrom;
                     
-                if (mail($to, $subject, $message, $headers)) {
+                if (mail($to, $contactSub, $contactMess, $headers)) {
                     echo "okay";
                 } else {
                     echo "error";
