ac8e7292c0770afba1beb389fbab573564984ecd
Michael Perron <michael.perron@g.austincc.edu>
2021-05-03 22:20:45 -0500

    changed the given foreach loop for a parser loop

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,11 +1,11 @@
-<h2><?= $title; ?></h2>
+{stories}
+    <hr>
+    <br>
+    {title}<br>
+    <br>
+    {text}<br>
+    <p><a href="/news/{slug}">View article</a></p>
+{/stories}
 
-<?php foreach ($news as $news_item): ?>
-
-        <h3><?= $news_item['title']; ?></h3>
-        <div class="main">
-                <?= $news_item['text']; ?>
-        </div>
-        <p><a href="<?= '/news/'.$news_item['slug'] ?>">View article</a></p>
-
-<?php endforeach; ?>
+<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
