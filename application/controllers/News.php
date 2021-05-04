4d7d1892b724349467bad8586281e56294adb06c
Michael Perron <michael.perron@g.austincc.edu>
2021-05-03 22:32:53 -0500

    adjusted several lines of code to facilitate queries from the database requesting arrays to be used in the stories loop that took the palce of the foreach loop. Also, added servertime variable to add server time to the footer

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -3,33 +3,43 @@ class News extends CI_Controller {
 
         public function __construct()
         {
-                parent::__construct();
-                $this->load->model('news_model');
-                $this->load->helper('url_helper');
+            parent::__construct();
+            $this->load->model('news_model');
+            $this->load->helper('url_helper');
         }
+
+
+
+
         public function view($slug = NULL)
 		{
-        $data['news_item'] = $this->news_model->get_news($slug);
+            $data['news_item'] = $this->news_model->get_news($slug);
+            $data['servertime'] = date('l jS \of F Y h:i:s A');//server time
 
         if (empty($data['news_item']))
         {
-                show_404();
+            show_404();
         }
 
-        $data['title'] = $data['news_item']['title'];
+            $data['title'] = $data['news_item']['title'];
+            $data['text'] = $data['news_item']['text'];
+            $data['slug'] = $data['news_item']['slug'];
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('news/view', $data);
-        $this->load->view('templates/footer');
+            $this->load->view('templates/header', $data);
+            $this->parser->parse('news/view', $data);
+            $this->load->view('templates/footer');
 		}
+
+
+
 		public function index()
 		{
-        $data['news'] = $this->news_model->get_news();
-        $data['title'] = 'News archive';
-        $data['servertime'] = date('l jS \of F Y h:i:s A');//server time
-
-        $this->load->view('templates/header', $data);
-        $this->load->view('news/index', $data);
-        $this->load->view('templates/footer');
+            $data['title'] = "News Archive";
+            $data['stories'] = $this->news_model->get_news();
+            $data['servertime'] = date('l jS \of F Y h:i:s A');//server time
+            
+            $this->load->view('templates/header', $data);
+            $this->parser->parse('news/index', $data);
+            $this->load->view('templates/footer', $data);
 		}
 }
