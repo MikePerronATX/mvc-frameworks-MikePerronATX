9bdc3c95b43fa750e68977d66fed9d0b6553b8fc
Michael Perron <michael.perron@g.austincc.edu>
2021-05-10 06:51:03 -0500

    added additional html structures for design

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,11 +1,16 @@
-{stories}
-    <hr>
-    <br>
-    {title}<br>
-    <br>
-    {text}<br>
-    <p><a href="/news/{slug}">View article</a></p>
-{/stories}
-
+<!--
+Name: Michael Perron
+Final Project
+Purpose: 
+-->
+<div class="egg-text">
+    <h1 class="mainColorTitle">News Archive</h1>
+    {stories}<hr><br>
+        <div class="newsArchTitle">{title}</div>
+        <br>
+        <img src="/assets/images/{slug}.jpg" class="img-fluid">
+        <p><a href="/news/{slug}">View article</a></p>
+    {/stories}
+</div>
 <script src="/assets/js/jquery.min.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
