be8f4d0ae2d279cf479faf9679aca48564ad6a56
Michael Perron <michael.perron@g.austincc.edu>
2021-05-10 06:52:07 -0500

    added additional html structure for design

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -3,13 +3,18 @@ Name: Michael Perron
 Final Project
 Purpose: Page to view output.
 -->
-<div class="viewDes">
-    {title}
+<div class= bodyNews>
+    <div class="newsTitle">
+        {title}
+    </div>
+    <div class="news_img">
+        <img src="/assets/images/{slug}.jpg" class="img-fluid">
+    </div>
+    <div class="viewText">
+        <div class="typewriter-Text">{text}<br><br>
+            <a href="/news">Go back to News Archive</a><br>
+        </div>
+    </div>
 </div>
-<div class="viewText">
-    {text}
-</div>
-<a href="/news">Go back to News Archive</a><br>
-
 <script src=/assets/js/jquery.min.js></script>
 <script src=/assets/js/bootstrap.min.js></script>
