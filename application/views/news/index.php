f83ebf9659a3e31524163efe352a06364bd9fb5a
Michael Perron <michael.perron@g.austincc.edu>
2021-05-12 20:55:27 -0500

    adjusted top comment and added a div container line 9 and a div row line 10 for col-sm-4 colums to adjust size at certain screen widths

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,16 +1,24 @@
 <!--
 Name: Michael Perron
 Final Project
-Purpose: 
+Purpose: Display news archive 
 -->
+
 <div class="egg-text">
     <h1 class="mainColorTitle">News Archive</h1>
-    {stories}<hr><br>
-        <div class="newsArchTitle">{title}</div>
-        <br>
-        <img src="/assets/images/{slug}.jpg" class="img-fluid">
-        <p><a href="/news/{slug}">View article</a></p>
-    {/stories}
+    <div class="container">
+        <div class="row">
+            {stories}
+                <div class= "col-sm-4">
+                    <div class="newsArchTitle">{title}<br>
+                        <img src="/assets/images/{slug}.jpg" class="img-fluid">
+                        <p><a href="/news/{slug}">View article</a></p>
+                    </div>
+                </div>
+            {/stories}
+        </div>
+    </div>
 </div>
+
 <script src="/assets/js/jquery.min.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
